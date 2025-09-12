63b60d5e0f90a172cc7e8f425f1d96c02e353d4c
Yunze Xu <xyzinfernity@163.com>
2025-09-12 17:37:09 +0800

    Add recycler and reflection benchmarks

---
+++ src/main/java/io/bewaremypower/RecyclerBenchmark.java
@@ -0,0 +1,70 @@
+package io.bewaremypower;
+
+import io.netty.util.Recycler;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+record Tuple(int i, String s, double d) {}
+
+class RecycleTuple {
+
+  private static final Recycler<RecycleTuple> RECYCLER =
+      new Recycler<>() {
+        @Override
+        protected RecycleTuple newObject(Recycler.Handle<RecycleTuple> handle) {
+          return new RecycleTuple(handle);
+        }
+      };
+
+  private final Recycler.Handle<RecycleTuple> handle;
+  private int i;
+  private String s;
+  private double d;
+
+  static RecycleTuple get(int i, String s, double d) {
+    final var tuple = RECYCLER.get();
+    tuple.i = i;
+    tuple.s = s;
+    tuple.d = d;
+    return tuple;
+  }
+
+  private RecycleTuple(Recycler.Handle<RecycleTuple> handle) {
+    this.handle = handle;
+  }
+
+  void recycle() {
+    i = 0;
+    s = null;
+    d = 0.0;
+  }
+}
+
+public class RecyclerBenchmark {
+
+  @Warmup(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+  @Measurement(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+  @Benchmark
+  public void testRecycler() {
+    final var tuple = RecycleTuple.get(1, "hello", 3.14);
+    tuple.recycle();
+  }
+
+  @Warmup(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+  @Measurement(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+  @Benchmark
+  public void testRecord() {
+    final var tuple = new Tuple(1, "hello", 3.14);
+  }
+
+  public static void main(String[] args) throws RunnerException {
+    final var opt =
+        new OptionsBuilder().include(RecyclerBenchmark.class.getSimpleName()).forks(1).build();
+    new Runner(opt).run();
+  }
+}
---
+++ src/main/java/io/bewaremypower/ReflectionBenchmark.java
@@ -0,0 +1,84 @@
+package io.bewaremypower;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+class Sender {
+
+  private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+  private synchronized int internalAsyncSend(int x) {
+    return x;
+  }
+
+  void asyncSend(int x) {
+    executor.execute(() -> internalAsyncSend(x));
+  }
+
+  public void shutdown() {
+    executor.shutdown();
+  }
+}
+
+@State(Scope.Benchmark)
+public class ReflectionBenchmark {
+
+  @State(Scope.Benchmark)
+  public static class Input {
+    private final Sender sender = new Sender();
+    Method method;
+  }
+
+  @Setup(Level.Trial)
+  public void setup(Input input) throws Exception {
+    // input.method = Sender.class.getDeclaredMethod("internalAsyncSend", int.class);
+    // input.method.setAccessible(true);
+  }
+
+  @TearDown(Level.Trial)
+  public void teardown(Input input) throws Exception {
+    input.sender.shutdown();
+  }
+
+  @Warmup(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+  @Measurement(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+  @Benchmark
+  public void testReflection(Input input) {
+    try {
+      input.method = Sender.class.getDeclaredMethod("internalAsyncSend", int.class);
+      input.method.setAccessible(true);
+      input.method.invoke(input.sender, 100);
+    } catch (IllegalAccessException | InvocationTargetException e) {
+      throw new RuntimeException(e);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Warmup(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+  @Measurement(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+  @Benchmark
+  public void testThreadSwitch(Input input) {
+    input.sender.asyncSend(100);
+  }
+
+  public static void main(String[] args) throws RunnerException {
+    final var opt =
+        new OptionsBuilder().include(ReflectionBenchmark.class.getSimpleName()).forks(1).build();
+    new Runner(opt).run();
+  }
+}
